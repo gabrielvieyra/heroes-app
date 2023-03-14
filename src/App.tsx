8ad9fd24c421f45576fded46fd76a1265f622a7c
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2023-03-14 18:12:08 -0300

    agrego las rutas que identifique para el front

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-function App() {
-  let name: string = 'Gabriel';
-  let surname: string = 'Vieyra';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
+// Components
+import { Navbar } from './components/index';
+
+// Pages
+import { Marvel, Dc, Hero } from './pages/index';
+
+// Styles
+import './App.scss';
+
+function App() {
   return (
     <>
-      <h1>Apellido: {surname}</h1>
-      <h2>Nombre: {name}</h2>
+      <Navbar />
+
+      <Routes>
+        <Route path='/marvel' element={<Marvel />} />
+        <Route path='/dc' element={<Dc />} />
+
+        <Route path='/hero' element={<Hero />} />
+
+        <Route path='/' element={<Navigate to='/marvel' />} />
+      </Routes>
     </>
   );
 }
