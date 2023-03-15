8d9bf2cb30a65f3be61e0e6d9cf7d7285982be5e
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2023-03-15 12:08:22 -0300

    agrego la pantalla DC

---
+++ src/pages/Dc/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+// Components
+import { HeroeCardsContainer } from '../../components';
+
+// Styles
+import './styles.scss';
+
+const Dc: FC = () => {
+  return <HeroeCardsContainer title='DC Comics' publisher='DC Comics' />;
+};
+
+export default Dc;
