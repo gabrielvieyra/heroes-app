79c06318dea01f82228f43cfec1acdfeecbcb697
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2023-06-02 16:18:43 -0300

    agrego nuevas variantes al componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,9 +3,13 @@ import { FC, ReactNode } from 'react';
 // Styles
 import './styles.scss';
 
+type ButtonVariant = 'primary' | 'secundary' | 'delete';
+type ButtonType = 'button' | 'submit';
+
 interface ButtonProps {
   children: ReactNode;
-  type?: 'button' | 'submit';
+  type?: ButtonType;
+  variant?: ButtonVariant;
   onClick?: () => void;
   disabled?: boolean;
 }
@@ -13,11 +17,17 @@ interface ButtonProps {
 export const Button: FC<ButtonProps> = ({
   children,
   type = 'button',
+  variant = 'primary',
   onClick,
   disabled = false,
 }) => {
   return (
-    <button type={type} className='button' onClick={onClick} disabled={disabled}>
+    <button
+      type={type}
+      className={`button button--${variant}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
