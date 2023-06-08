6f9440661b38817e68fe59e9a0820159a018a8e0
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2023-06-08 19:29:35 -0300

    elimino helpers que no se utilizan

--- src/helpers/getHeroById.ts
+++
@@ -1,9 +0,0 @@
-// Data
-import { heroes } from '../data/heroes';
-
-// Interfaces
-import { Heroe } from '../types/types';
-
-export const getHeroById = (id: string): Heroe => {
-  return heroes.find(hero => hero.id === id)!;
-};
--- src/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher';
-import { getHeroById } from '../helpers/getHeroById';
-import { getHeroesByName } from '../helpers/getHeroesByName';
-
-export { getHeroesByPublisher, getHeroById, getHeroesByName };
