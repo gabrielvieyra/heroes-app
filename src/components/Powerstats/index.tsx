933fe2c00a63825b8389c588bd85844f8955a29c
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2023-06-18 17:51:24 -0300

    agrego nueva variante en el componente Powerstats

--- src/components/Powerstats/index.tsx
+++ src/components/Powerstats/index.tsx
@@ -1,4 +1,10 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+
+// Components
+import { ProgressBar } from '../../components/index';
+
+// Context
+import { HeroesContext } from '../../context/HeroesContext';
 
 // Interfaces
 import { PowerstatsI } from '../../types/types';
@@ -6,26 +12,34 @@ import { PowerstatsI } from '../../types/types';
 // Styles
 import './styles.scss';
 
+type PowerstatsVariant = 'default' | 'fullHorizontal';
+
 interface PowerstatsProps {
   powerstats: PowerstatsI;
+  variant?: PowerstatsVariant;
 }
 
-export const Powerstats: FC<PowerstatsProps> = ({ powerstats }) => {
+export const Powerstats: FC<PowerstatsProps> = ({ powerstats, variant = 'default' }) => {
+  const { team } = useContext(HeroesContext);
+
   return (
     <div className='powerstats'>
       {Object.entries(powerstats).map(([title, value]) => {
-        return (
+        return variant === 'default' ? (
           <div className='powerstats__container' key={title}>
             <div className='powerstats__container-wrapper'>
               <span>{title}</span>
               <span>{value}</span>
             </div>
-            <div className='powerstats__container-progress-bar'>
-              <div
-                style={{ width: `${value}%` }}
-                className='powerstats__container-progress-bar-percent'
-              ></div>
+            <ProgressBar value={value} />
+          </div>
+        ) : (
+          <div className='powerstats__fullHorizontal' key={title}>
+            <div className='powerstats__fullHorizontal-container'>
+              <span>{title}</span>
+              <span>{value}</span>
             </div>
+            <ProgressBar value={(value / team.length).toString()} />
           </div>
         );
       })}
